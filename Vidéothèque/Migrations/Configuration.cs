732bf82d1abd4f0e97881f43a629c1741ee31584
Kristi Mihali <kristimihali3@gmail.com>
2020-11-09 11:58:26 +0100

    Connection avec la BD SQL EXPRESS

---
+++ Vidéothèque/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace Vidéothèque.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Vidéothèque.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Vidéothèque.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
